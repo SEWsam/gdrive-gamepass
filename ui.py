ca103f1ae0f822c61191d856e10748639e5dcf4e
Sam Wirth <49174351+SEWsam@users.noreply.github.com>
2021-03-18 12:43:55 -0500

    Copyright and project settings

--- ui.py
+++ ui.py
@@ -1,9 +1,20 @@
-"""
-Copyright (c) 2021 Samuel Wirth
+"""ui.py - UI interface for sync_module.
 
-Licensed under the MIT License. See LICENSE for more info.
-"""
+Copyright (C) 2021 Samuel Wirth
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import os
 import PyQt5
 import sys
