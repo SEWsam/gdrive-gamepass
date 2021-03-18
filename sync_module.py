6aff815edf116157171932f4f7cf1a5bc98d49aa
Sam Wirth <49174351+SEWsam@users.noreply.github.com>
2021-03-18 13:12:50 -0500

    finished updating copyright

--- sync_module.py
+++ sync_module.py
@@ -1,7 +1,19 @@
-"""
-Copyright (c) 2021 Samuel Wirth
+"""sync_module.py: Manages local and cloud game saves.
 
-Licensed under the MIT License. See LICENSE for more info.
+Copyright (C) 2021 Samuel Wirth
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import os
 from pydrive.auth import GoogleAuth
